ea58247f675055c7ca75eba45ee77a24f0025755
eguamuch <desarrollo02@gs1gt.org>
2024-05-31 15:48:38 -0600

    [add] estados-cuenta

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,18 @@
+
 // En Navbar.js o al inicio de PaginaPrincipal.js
-const Navbar = ({ onSolicitarTarjeta }) => {
+const Navbar = ({ onSolicitarTarjeta, onEstadoCuenta, onMovimientosTarjeta }) => {
     return (
       <nav style={{ backgroundColor: '#f0f0f0', padding: '10px', textAlign: 'center' }}>
         <button onClick={onSolicitarTarjeta} style={{ margin: '10px', padding: '8px 16px', fontSize: '16px', cursor: 'pointer' }}>
           Solicitar Tarjeta
         </button>
+        <button onClick={onEstadoCuenta} style={{ margin: '10px', padding: '8px 16px', fontSize: '16px', cursor: 'pointer' }}>
+          Estado Cuenta
+        </button>
+        <button onClick={onMovimientosTarjeta} style={{ margin: '10px', padding: '8px 16px', fontSize: '16px', cursor: 'pointer' }}>
+          Ver Movimientos de Tarjeta
+        </button>
+
       </nav>
     );
   };
