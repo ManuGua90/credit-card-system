b41ada2809a864b6541a35833748222e6c02366c
eguamuch <desarrollo02@gs1gt.org>
2024-05-31 23:29:00 -0600

    [add] bugfix usuario

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001', // Reemplaza con la URL de tu backend
+  baseURL: 'http://3.15.147.101:3001', // Reemplaza con la URL de tu backend
 });
 
 export default instance;
